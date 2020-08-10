31748ffa232c9ea9c87e94a4eae08e57dd1fdfee
Vivekachooz <vivekachooz@gmail.com>
2020-08-10 09:51:07 +0530

    mido: Import power_profile.xml
    Change-Id: I5cb071762ab50b9c9279ff2c37266e9a8189af9e

--- device.mk
+++ device.mk
@@ -9,6 +9,7 @@ $(call inherit-product, device/xiaomi/msm8953-common/msm8953.mk)
 
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
+    $(LOCAL_PATH)/overlay \
     $(LOCAL_PATH)/overlay-lineage
 
 PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += \
