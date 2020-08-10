27ab88d347543003a68384f7b004a79fe1daa290
Vivekachooz <vivekachooz@gmail.com>
2020-08-10 09:51:07 +0530

    mido: Add device specific overlays
    Change-Id: Ie4edbb2ac501bee0067bad2e6ab41aa41cb7a6ae

--- device.mk
+++ device.mk
@@ -7,5 +7,9 @@
 # Inherit from msm8953-common
 $(call inherit-product, device/xiaomi/msm8953-common/msm8953.mk)
 
+# Overlays
+DEVICE_PACKAGE_OVERLAYS += \
+    $(LOCAL_PATH)/overlay-lineage
+
 # Inherit proprietary files
 $(call inherit-product-if-exists, vendor/xiaomi/mido/mido-vendor.mk)
